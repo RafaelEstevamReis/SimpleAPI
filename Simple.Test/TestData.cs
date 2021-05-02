d4c177934334bdd4f65e86b1039053c18e1f0fc7
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-05-02 19:27:19 -0300

    Add test project

---
+++ Simple.Test/TestData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Simple.Test
+{
+    public class TestData
+    {
+        public int Id { get; set; }
+        public Guid Uid { get; set; }
+
+        public string Text { get; set; }
+        public int Number { get; set; }
+        public double DoubleFloatPoint { get; set; }
+    }
+}
---
+++ Simple.Test/TestResponse.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Simple.Test
+{
+    public class TestResponse
+    {
+        public Args args { get; set; }
+        public Headers headers { get; set; }
+        public string origin { get; set; }
+        public string url { get; set; }
+        public TestData json { get; set; }
+    }
+    public class Args { }
+
+    public class Headers
+    {
+        public string Accept { get; set; }
+        public string AcceptEncoding { get; set; }
+        public string AcceptLanguage { get; set; }
+        public string ContentType { get; set; }
+        public string Dnt { get; set; }
+        public string Host { get; set; }
+        public string Origin { get; set; }
+        public string Referer { get; set; }
+        public string UserAgent { get; set; }
+    }
+}
