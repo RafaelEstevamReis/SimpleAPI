16109193d82ab0e74867e405e2cea55800234635
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-01-27 17:53:49 -0300

    Create TestLogon.cs

---
+++ Simple.Test/TestLogon.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple.Test
+{
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Mock_ReturnSuccess { get; set; }
+
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
