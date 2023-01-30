3243a7d296527e91a806da4495d340cf79c3f3f1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-30 15:34:24 -0300

    Add UnsuccessfulStatusCodeException<T>

--- Simple.API/UnsuccessfulStatusCodeException.cs
+++ Simple.API/UnsuccessfulStatusCodeException.cs
@@ -12,4 +12,16 @@ namespace Simple.API
 
         public Response Response { get; }
     }
+    public class UnsuccessfulStatusCodeException<T> : HttpRequestException
+    {
+        public UnsuccessfulStatusCodeException(Response response, T error)
+            : base($"[{response.RequestMessage.Method}] {response.RequestMessage.RequestUri} [{(int)response.StatusCode}] failed with {response.ReasonPhrase}")
+        {
+            Response = response;
+            ErrorInformation = error;
+        }
+
+        public Response Response { get; }
+        public T ErrorInformation { get; }
+    }
 }
