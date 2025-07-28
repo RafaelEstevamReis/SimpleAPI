f010f6d940b437d2c51ab03da44d18805431571c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-07-28 13:19:23 -0300

    Add ClientBuilder sample

---
+++ Simple.Test/Example_ClientBuilder.cs
@@ -0,0 +1,55 @@
+﻿namespace Simple.Test;
+
+using Simple.API;
+using Simple.API.ClientBuilderAttributes;
+using System;
+using System.Threading.Tasks;
+
+public class Example_ClientBuilder
+{
+    internal static async Task Run()
+    {
+        var demoClient = ClientBuilder.Create<IDemo>("https://httpbin.org/");
+
+        var resultGet = await demoClient.GetAnythingAsync();
+        var resultNPost = await demoClient.PostNothingAsync();
+        var resultPost = await demoClient.PostAnythingAsync(new TestData
+        {
+            Number = 42
+        });
+        var resultPut = await demoClient.PutAnythingAsync(new TestData
+        {
+            Number = 42
+        });
+
+        // Using GetSuccessfulData on return
+        var resultPostD = await demoClient.PostAnythingSuccessfulAsync(new TestData
+        {
+            Number = 42
+        });
+
+        ;
+        Console.WriteLine("End");
+    }
+
+    public interface IDemo
+    {
+        /* Normal Respnse return */
+        [Get("anything")]
+        Task<Response<TestResponse>> GetAnythingAsync();
+
+        [Post("anything")]
+        Task<Response<TestResponse>> PostNothingAsync();
+
+        [Post("anything")]
+        Task<Response<TestResponse>> PostAnythingAsync(TestData d);
+
+        [Put("anything")]
+        Task<Response<TestResponse>> PutAnythingAsync(TestData d);
+
+        /* Validate with GetSuccessfulData */
+        [Post("anything")]
+        Task<TestResponse> PostAnythingSuccessfulAsync(TestData d);
+    }
+
+}
