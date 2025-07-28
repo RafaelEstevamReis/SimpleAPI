39202d9c1a655cb9276150b3b624f7ad811541c0
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-07-28 12:27:38 -0300

    Create ClientBuilderAttributes

---
+++ Simple.API/ClientBuilderAttributes/Attributes.cs
@@ -0,0 +1,31 @@
+﻿namespace Simple.API.ClientBuilderAttributes;
+
+using System;
+
+
+public class MethodAttribute : Attribute
+{
+    public string Route { get; protected set; }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class GetAttribute : MethodAttribute
+{
+    public GetAttribute(string route) => Route = route;
+}
+[AttributeUsage(AttributeTargets.Method)]
+public class PostAttribute : MethodAttribute
+{
+    public PostAttribute(string route) => Route = route;
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class PutAttribute : MethodAttribute
+{
+    public PutAttribute(string route) => Route = route;
+}
+[AttributeUsage(AttributeTargets.Method)]
+public class DeleteAttribute : MethodAttribute
+{
+    public DeleteAttribute(string route) => Route = route;
+}
