449445687d626dfa70b57a7c5972f68567454903
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-09-14 19:13:16 -0300

    Refatora ClientBuilder

--- Simple.API/ClientBuilder.cs
+++ Simple.API/ClientBuilder.cs
@@ -15,7 +15,18 @@ public class ClientBuilder : DispatchProxy
     static readonly Type TypeOfResponseExtensions = typeof(ResponseExtensions);
 
     static readonly MethodInfo[] MethodsOfClientInfo = TypeOfClientInfo.GetMethods();
-    static readonly MethodInfo[] MethodsOfResponseExtensions = TypeOfResponseExtensions.GetMethods();
+    static readonly MethodInfo MethodGetAsync = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.GetAsync)).First();
+    static readonly MethodInfo MethodPostAsync = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.PostAsync)).First();
+    static readonly MethodInfo MethodPutAsync = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.PutAsync)).First();
+
+    static readonly MethodInfo MethodGetSuccessfulDataTask = TypeOfResponseExtensions
+        .GetMethods()
+        .Where(o => o.Name == nameof(ResponseExtensions.GetSuccessfulData)
+                    && o.ReturnType.BaseType.Name == "Task")
+        .First(); // Exception if not found
+
+
+
 
     internal ClientInfo client;
     public ClientBuilder()
@@ -42,49 +53,28 @@ public class ClientBuilder : DispatchProxy
     protected override object Invoke(MethodInfo targetMethod, object[] args)
     {
         // Internal Methods
-        if (targetMethod.Name == "GetInternalClient")
+        if (processInternalFunctions(targetMethod, args, out object intRet))
         {
-            return client;
-        }
-        if (targetMethod.Name == "SetAuthorizationBearer")
-        {
-            if (args.Length != 1) throw new ArgumentException("Expcted `string` bearer parameter");
-
-            client.SetAuthorizationBearer((string)args[0]);
-            return null; // void
-        }
-        if (targetMethod.Name == "SetHeader")
-        {
-            if (args.Length != 2) throw new ArgumentException("Expcted `string` header key and value parameters");
-
-            client.SetHeader((string)args[0], (string)args[1]);
-            return null; // void
+            return intRet;
         }
 
-        // Get the [METHOD] attribute
-        MethodAttribute getAttr = targetMethod.GetCustomAttribute<GetAttribute>();
-        MethodAttribute postAttr = targetMethod.GetCustomAttribute<PostAttribute>();
-        MethodAttribute putAttr = targetMethod.GetCustomAttribute<PutAttribute>();
-        //MethodAttribute deleteAttr = targetMethod.GetCustomAttribute<DeleteAttribute>();
-
-        MethodAttribute httpMethod = getAttr
-                                    ?? postAttr
-                                    ?? putAttr
-                                    //?? deleteAttr
-                                    ;
+        /* Validations */
+        MethodAttribute httpMethod = getHttpMethodAttribute(targetMethod);
         if (httpMethod == null) throw new InvalidOperationException($"Method {targetMethod.Name} lacks MethodAttribute");
 
         // Ensure the method returns a Task<T>
         if (!TypeOfTask.IsAssignableFrom(targetMethod.ReturnType)) throw new InvalidOperationException($"Method {targetMethod.Name} must return a Task");
 
+        /* Return Type */
         // Get the return type (e.g., Response<TestResponse> from Task<Response<TestResponse>>)
         var taskReturnType = targetMethod.ReturnType.IsGenericType ? targetMethod.ReturnType.GetGenericArguments()[0] : null;
 
-        // Detect if RESPONSE or DIRECT
+        // Detect if Response<T> or <T>
         Type innerType;
         bool usesResponseReturn;
         if (taskReturnType == null || !taskReturnType.IsGenericType || taskReturnType.GetGenericTypeDefinition() != typeof(Response<>))
         {
+            // Returns Task<T>, will call GetSuccessfulData<T>()
             innerType = taskReturnType;
             usesResponseReturn = false;
         }
@@ -95,49 +85,91 @@ public class ClientBuilder : DispatchProxy
             usesResponseReturn = true;
         }
 
-        // Call ClientInfo.GetAsync<T> dynamically with the inner type
-        MethodInfo methodToCall;
-        object[] methodArgs;
+        /* Extract Route information */
 
-        if (getAttr != null)
-        {
-            var allMethods = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.GetAsync)).ToArray();
-            methodToCall = allMethods.FirstOrDefault().MakeGenericMethod(innerType);
 
-            if (args.Length == 0)
-            {
-                methodArgs = [httpMethod.Route];
-            }
-            else methodArgs = [httpMethod.Route, args[0]];
-        }
-        else if (postAttr != null)
-        {
-            var allMethods = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.PostAsync)).ToArray();
-            methodToCall = allMethods.FirstOrDefault().MakeGenericMethod(innerType);
-
-            if (args.Length == 0) methodArgs = [httpMethod.Route, null];
-            else methodArgs = [httpMethod.Route, args[0]];
-        }
-        else if (putAttr != null)
-        {
-            var allMethods = MethodsOfClientInfo.Where(o => o.Name == nameof(ClientInfo.PutAsync)).ToArray();
-            methodToCall = allMethods.FirstOrDefault().MakeGenericMethod(innerType);
-
-            if (args.Length == 0) methodArgs = [httpMethod.Route, null];
-            else methodArgs = [httpMethod.Route, args[0]];
-        }
-        else throw new NotSupportedException("HttpMethod not supported");
-
+        /* Method Selection */
+        selectMethodToExecute(args, httpMethod, innerType, out MethodInfo methodToCall, out object[] methodArgs);
+        // Call ClientInfo.[Method]Async<T> dynamically with the inner type
         var task = (Task)methodToCall.Invoke(client, methodArgs);
 
+        // Calls GetSuccessfulData
         if (!usesResponseReturn)
         {
-            var getSuccessfulDataMethod = MethodsOfResponseExtensions.FirstOrDefault(o => o.ReturnType.BaseType.Name == "Task").MakeGenericMethod(innerType);
+            var getSuccessfulDataMethod = MethodGetSuccessfulDataTask.MakeGenericMethod(innerType);
             task = (Task)getSuccessfulDataMethod.Invoke(null, [task]);
         }
 
         return task;
     }
+
+    private static void selectMethodToExecute(object[] args, MethodAttribute httpMethod, Type innerType, out MethodInfo methodToCall, out object[] methodArgs)
+    {
+        if (httpMethod is GetAttribute)
+        {
+            methodToCall = MethodGetAsync.MakeGenericMethod(innerType);
+
+            if (args.Length == 0) methodArgs = [httpMethod.Route];
+            else methodArgs = [httpMethod.Route, args[0]];
+        }
+        else if (httpMethod is PostAttribute)
+        {
+            methodToCall = MethodPostAsync.MakeGenericMethod(innerType);
+
+            if (args.Length == 0) methodArgs = [httpMethod.Route, null];
+            else methodArgs = [httpMethod.Route, args[0]];
+        }
+        else if (httpMethod is PutAttribute)
+        {
+            methodToCall = MethodPutAsync.MakeGenericMethod(innerType);
+
+            if (args.Length == 0) methodArgs = [httpMethod.Route, null];
+            else methodArgs = [httpMethod.Route, args[0]];
+        }
+        else throw new NotSupportedException("HttpMethod not supported");
+    }
+
+    private static MethodAttribute getHttpMethodAttribute(MethodInfo targetMethod)
+    {
+        // Get the [METHOD] attribute
+        MethodAttribute getAttr = targetMethod.GetCustomAttribute<GetAttribute>();
+        MethodAttribute postAttr = targetMethod.GetCustomAttribute<PostAttribute>();
+        MethodAttribute putAttr = targetMethod.GetCustomAttribute<PutAttribute>();
+        MethodAttribute deleteAttr = targetMethod.GetCustomAttribute<DeleteAttribute>();
+
+        return getAttr ?? postAttr ?? putAttr ?? deleteAttr;
+    }
+
+    private bool processInternalFunctions(MethodInfo targetMethod, object[] args, out object intRet)
+    {
+        // Process Internal Functions
+        if (targetMethod.Name == "GetInternalClient")
+        {
+            intRet = client;
+            return true;
+        }
+        if (targetMethod.Name == "SetAuthorizationBearer")
+        {
+            if (args.Length != 1) throw new ArgumentException("Expcted `string` bearer parameter");
+
+            client.SetAuthorizationBearer((string)args[0]);
+            intRet = null; // void
+            return true;
+        }
+        if (targetMethod.Name == "SetHeader")
+        {
+            if (args.Length != 2) throw new ArgumentException("Expcted `string` header key and value parameters");
+
+            client.SetHeader((string)args[0], (string)args[1]);
+            intRet = null; // void
+            return true;
+        }
+
+        // No more internals
+        intRet = null;
+        return false;
+    }
+
 }
 
 #endif
