aec18bef557634bd775f5d73ffb4b48dbfde380d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-02-24 15:47:25 -0300

    Update startup for new openapi

--- Simple.TestWebApi/Startup.cs
+++ Simple.TestWebApi/Startup.cs
@@ -1,3 +1,5 @@
+namespace Simple.TestWebApi;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -7,93 +9,90 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
 using System.Text;
 
-namespace Simple.TestWebApi
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        Configuration = configuration;
+    }
 
-        public IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // API stuff
-            services.AddControllers();
-            // Auth stuff
-            var key = Encoding.ASCII.GetBytes(Auth.Token.Secret);
-            services.AddAuthentication(x =>
-                    {
-                        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                    })
-                    .AddJwtBearer(x =>
-                    {
-                        x.RequireHttpsMetadata = false;
-                        x.SaveToken = true;
-                        x.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateIssuerSigningKey = true,
-                            IssuerSigningKey = new SymmetricSecurityKey(key),
-                            ValidateIssuer = false,
-                            ValidateAudience = false
-                        };
-                    });
-            // Swagger stuff
-            services.AddSwaggerGen(c =>
-            {
-                var schemeId = JwtBearerDefaults.AuthenticationScheme;
-
-                // 1. Criamos o esquema SEM a propriedade Reference (que não existe mais)
-                var jwtSecurityScheme = new OpenApiSecurityScheme
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // API stuff
+        services.AddControllers();
+        // Auth stuff
+        var key = Encoding.ASCII.GetBytes(Auth.Token.Secret);
+        services.AddAuthentication(x =>
                 {
-                    Scheme = "bearer",
-                    BearerFormat = "JWT",
-                    Name = "Authorization",
-                    In = ParameterLocation.Header,
-                    Type = SecuritySchemeType.Http,
-                    Description = "Put **_ONLY_** your JWT Bearer token on textbox below!"
-                };
-
-                // 2. Registramos a definição usando o ID
-                c.AddSecurityDefinition(schemeId, jwtSecurityScheme);
-
-                // 3. Criamos o requisito usando a nova classe de referência
-                c.AddSecurityRequirement(document =>
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
                 {
-                    OpenApiSecuritySchemeReference schemeRef = new("Bearer");
-                    OpenApiSecurityRequirement requirement = new()
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
                     {
-                        [schemeRef] = []
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
                     };
-                    return requirement;
                 });
-
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Simple.TestWebApi", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // Swagger stuff
+        services.AddSwaggerGen(c =>
         {
-            // aways use swagger, this is a test app....
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple.TestWebApi v1"));
+            var schemeId = JwtBearerDefaults.AuthenticationScheme;
 
-            // normal stuff
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            // 1. Criamos o esquema SEM a propriedade Reference (que não existe mais)
+            var jwtSecurityScheme = new OpenApiSecurityScheme
             {
-                endpoints.MapControllers();
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Description = "Put **_ONLY_** your JWT Bearer token on textbox below!"
+            };
+
+            // 2. Registramos a definição usando o ID
+            c.AddSecurityDefinition(schemeId, jwtSecurityScheme);
+
+            // 3. Criamos o requisito usando a nova classe de referência
+            c.AddSecurityRequirement(document =>
+            {
+                OpenApiSecuritySchemeReference schemeRef = new("Bearer");
+                OpenApiSecurityRequirement requirement = new()
+                {
+                    [schemeRef] = []
+                };
+                return requirement;
             });
-        }
+
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Simple.TestWebApi", Version = "v1" });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // aways use swagger, this is a test app....
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple.TestWebApi v1"));
+
+        // normal stuff
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
     }
 }
